d18b2686e9fd813039b69b59688ee37d80c89513
Thomas <andrewtbonnema@gmail.com>
2017-11-14 14:13:24 -0800

    commit Updating Script and removing other scripts

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,9 @@
+//JQUERY INTRODUCTION
 // Test code for the functioning of the Server;
-
 $(document).ready(function() {
 
-    $('select').material_select();
-    // Trying to interperet JSONP response;
+
+    // Trying to interperet JSONP response for Edamam API;
     function jsonCallback(json) {
         console.log(json);
     }
@@ -12,42 +12,63 @@ $(document).ready(function() {
         console.log(json);
     }
 
-    //Build queryURL based on variables selected from menus which will replace the healthLabel function; 
-    //Figure out how to combine multiple API's;
-    function searchRecipe(searchFood) {
 
-        // API Keys, ID, and variables for Nutrition Analysis;
-        var APINutKey = "3413a3675f84f8490cefcc722d559d39";
-        var APINutID = "be4b8311";
+    //CREATE CLICK FUNCTION TO RUN API FUNCTIONS;
+
+    //SEPEARTING EDAMAM API
+    function searchRecipe(searchFood) {
+        //GLOBAL EDAMAM VARIABLES;
+        //var apiNutKey = "3413a3675f84f8490cefcc722d559d39";
+        //var apiNutID = "be4b8311";
+
+        //EDAMAM KEYS;
+        var apiRecKey = "e8b036bdd9a6347775ea2c30690c2d94";
+        var apiRecID = "7cd61c30";
+
+        //SEARCH INPUT;
         var searchFood = $("input").eq(0).val();
         var dummySearch = "banana";
-        // API Keys, ID, and variables for Recipes based on drop-down paramaters;
-        var APIRecKey = "e8b036bdd9a6347775ea2c30690c2d94";
-        var APIRecID = "7cd61c30";
+
+        //VITAMIN DEFICIENCIES INPUT;
+        var vitaminCount = $(".def").val().trim();
+        
+        //INDEX DISPLAY TOTAL = 10;
         var fromIndex = 0;
         var toIndex = 10;
+        
+        //CALORIE INPUT;
         var caloriesMin = "gte%0";
         var caloriesMax = "lte%20722";
+
+        //HEALTH INPUT FUNCTION (VEGATARIAN, VEGAN, OMNIVORE);
+        var health = "vegan"; 
+
+        //JSONP CALLBACK FUNCTION
         var callback = "?";
-        var health = "vegan"; // KEY! Vegetarian,  vegan, etc...;
-        //queryURL creation; The callback function allows for the jsonp data type to bypass the problem of no CORS implementation;
-        var queryURL = "https://api.edamam.com/search?q=" + dummySearch + "&app_id=" + APIRecID + "&app_key=" + APIRecKey + "&from="+ fromIndex + "&to=" + toIndex + "&calories=" + caloriesMin + ",%20" + caloriesMax + "&health=" + health + "&callback=food";
+
+        //queryURL creation
+        var queryURL = "https://api.edamam.com/search?q=" + dummySearch + "&app_id=" + apiRecID + "&app_key=" + apiRecKey + "&from="+ fromIndex + "&to=" + toIndex + "&calories=" + caloriesMin + ",%20" + caloriesMax + "&health=" + health + "&callback=food";
         console.log(queryURL);
-        // AJAX Pull;
+        // AJAX Pull
         $.ajax({
             url: queryURL,
             method: "GET",
             jsonpCallback: "food",
             dataType: "jsonp",
+            data: {
+
+            }
         },
         )
 
+        //DISPLAY DATA
         .then(data => {
+            data.hits.forEach(nom => $('.data-header').append(`<p>${nom.recipe.label}</p> <img src=${nom.recipe.image}></img>`));
             console.log(data.hits);
-            data.hits.forEach(nom => $('.intro').append(`<p>${nom.recipe.label}</p>`));
         },
         )
 
+        //DONE FUNCTION TO RECORD RESPONSE
         .done(function(response) {
             // Log the queryURL;
             console.log(queryURL);
@@ -56,60 +77,57 @@ $(document).ready(function() {
         });
     };
 
-    function searchGooglePlaces(searchMap) {
-        //Places Libraries
-        var map;
-        var service;
-        var infowindow;
-        
-        function initialize() {
-          var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
-        
-          map = new google.maps.Map(document.getElementById('map'), {
-              center: pyrmont,
-              zoom: 15
-            });
-        
-          var request = {
-            location: pyrmont,
-            radius: '500',
-            query: 'restaurant'
-          };
-          
-          service = new google.maps.places.PlacesService(map);
-          service.textSearch(request, callback);
-        }
-        
-        function callback(results, status) {
-          if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-              var place = results[i];
-              createMarker(results[i]);
-            }
-          }
-        };
-        console.log(results);    
-        
-    }; 
+
+
+    //SEPERATING ZOMATO API;
     function searchZomato(searchRest) {
+        
+        var lat = "";
+        var long = "";
+        //GEOLOCATION HTML API PULL;
+        function getLocation() {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        };
+        function showPosition(position) {
+            console.log(position);
+            lat = position.coords.latitude; 
+            long = position.coords.longitude;
+            console.log(position.coords);
+            console.log(long);
+            console.log(position);
+
+        };
+        // FIND NEAREST RESTERAUNT
+        getLocation();
+        showPosition();
+
+        //GLOBAL VARIABLES;
+        //GEOLOCATION;
+
+
+        //HEALTH INPUT FUNCTION (VEGATARIAN, VEGAN, OMNIVORE);
+        var healthSearch = "";
+
+        //queryURL creation
+        var queryURL = "https://developers.zomato.com/api/v2.1/" + "geocode?lat=" + lat + "&lon=" + long;
+        console.log(queryURL);
+
+        //AJAX Pull
         $.ajax({
             type: "GET", 
             headers: {
-            'X-Zomato-API-Key': 'c7395f5b6224146e27ac3b2feb756dd7' //non-standard header as researched
+            'X-Zomato-API-Key': 'c7395f5b6224146e27ac3b2feb756dd7' 
             },
-            url: 'https://developers.zomato.com/api/v2.1/dailymenu', 
-            dataType: 'json', 
-            data: {
-            //could be directly in URL, but this is more pretty, clear and easier to edit
-            res_id: 'YOUR_RESTAURANT_OR_PLACE_ID',
-            },
-            processData: true, //data is an object => tells jQuery to construct URL params from it
+            url: queryURL, 
+            dataType: 'json',
+            processData: true, 
             success: function(data) {
-            console.log(data); //logging response data
+            console.log(data);
             }
         });
-    }
+    };
+
+    //RUN FUNCTIONS
     searchZomato();
     searchRecipe();
-    searchGooglePlaces();
 })
